fc39d0a9273c7b9d55b2c333ffdec1bbd6fedecc
mayurg29 <97237587+mayurg29@users.noreply.github.com>
2022-04-06 21:24:43 +0530

    Watch Service added

---
+++ WatchService/NIOFileAPITest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.IOStreams;
+
+import com.bridgelabz.Day27.Java8WatchServiceExample;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class NIOFileAPITest {
+    private static String HOME = System.getProperty("user.home");
+    private static String PLAY_WITH_NIO = "TempPlayGround";
+
+    @Test
+    public void givenADirectory_WhenWatched_ListAllTheActivities() throws IOException {
+        Path dir = Paths.get(HOME + "/" + PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExample(dir).processEvents();
+    }
+}
