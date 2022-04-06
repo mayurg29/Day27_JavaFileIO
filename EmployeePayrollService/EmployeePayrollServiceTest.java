459fcffb1eac0591599a971a53f2459bfeead724
mayurg29 <97237587+mayurg29@users.noreply.github.com>
2022-04-06 21:23:45 +0530

    Employee Payroll added

---
+++ EmployeePayrollService/EmployeePayrollServiceTest.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.IOStreams;
+
+import com.bridgelabz.Day27.EmployeePayrollData;
+import com.bridgelabz.Day27.EmployeePayrollService;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static com.bridgelabz.Day27.EmployeePayrollService.IOService.FILE_IO;
+
+public class EmployeePayrollServiceTest {
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arrayOfEmps = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+        employeePayrollService.writeEmployeePayrollData(FILE_IO);
+        employeePayrollService.printData(FILE_IO);
+        long entries = employeePayrollService.countEntries(FILE_IO);
+        Assert.assertEquals(3, entries);
+    }
+}
